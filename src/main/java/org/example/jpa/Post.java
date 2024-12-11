4ce2002f19b8f7f1e36caa3e0ca93408ef452c1f
sangxxjin <sj_0368@naver.com>
2024-12-11 12:17:03 +0900

    게시글 테이블 생성

---
+++ src/main/java/org/example/jpa/Post.java
@@ -0,0 +1,27 @@
+package org.example.jpa;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Post {
+
+    //long => null X
+    //Long => null o
+    //Jpa 엔티티 클래스 특성상 id 필드는 null이 가능하도록
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    @Column(length = 100)
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+}
