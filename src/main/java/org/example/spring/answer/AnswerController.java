4a4fbd81fc67e814d0e60c5e63884056a797f9b1
sangxxjin <sj_0368@naver.com>
2024-12-13 01:26:34 +0900

    feat answerUpdate

--- src/main/java/org/example/spring/answer/AnswerController.java
+++ src/main/java/org/example/spring/answer/AnswerController.java
@@ -7,13 +7,16 @@ import org.example.spring.question.Question;
 import org.example.spring.question.QuestionService;
 import org.example.spring.user.SiteUser;
 import org.example.spring.user.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -36,5 +39,20 @@ public class AnswerController {
         }
         this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+        @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 }
--- src/main/java/org/example/spring/answer/AnswerService.java
+++ src/main/java/org/example/spring/answer/AnswerService.java
@@ -1,7 +1,9 @@
 package org.example.spring.answer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.example.spring.DataNotFoundException;
 import org.example.spring.question.Question;
 import org.example.spring.user.SiteUser;
 import org.springframework.stereotype.Service;
@@ -21,5 +23,19 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
     }
 }
