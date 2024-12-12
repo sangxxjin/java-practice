19a16fca1e7dcd32a14e9951667f5d853d0ac6ab
sangxxjin <sj_0368@naver.com>
2024-12-13 01:29:01 +0900

    feat answerDelete, show modifiedAt

--- src/main/java/org/example/spring/answer/AnswerService.java
+++ src/main/java/org/example/spring/answer/AnswerService.java
@@ -38,4 +38,7 @@ public class AnswerService {
         answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
+    }
 }
