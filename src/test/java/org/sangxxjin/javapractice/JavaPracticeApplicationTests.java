d0f7a6a32b8a26047bb72823c6f2df9dbe30389f
sangxxjin <sj_0368@naver.com>
2025-01-07 11:31:10 +0900

    setting

---
+++ src/test/java/org/sangxxjin/javapractice/JavaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sangxxjin.javapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
