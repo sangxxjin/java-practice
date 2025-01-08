0d41a3f3110e411cb153c3007fd9222bb930a0d1
sangxxjin <sj_0368@naver.com>
2025-01-08 14:03:25 +0900

    feat: 현재 요청이 user1 이 보낸 요청임을 강제로 인증처리

--- src/main/java/org/sangxxjin/javapractice/global/rq/Rq.java
+++ src/main/java/org/sangxxjin/javapractice/global/rq/Rq.java
@@ -1,5 +1,6 @@
 package org.sangxxjin.javapractice.global.rq;
 
+import java.util.List;
 import org.sangxxjin.javapractice.domain.member.member.entity.Member;
 import org.sangxxjin.javapractice.domain.member.member.service.MemberService;
 import org.sangxxjin.javapractice.global.exceptions.ServiceException;
@@ -7,6 +8,11 @@ import org.sangxxjin.javapractice.standard.util.Ut;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -16,27 +22,47 @@ import org.springframework.web.context.annotation.RequestScope;
 @Component
 @RequiredArgsConstructor
 public class Rq {
+
     private final MemberService memberService;
     private final HttpServletRequest request;
 
     public Member checkAuthentication() {
         String credentials = request.getHeader("Authorization");
         String apiKey = credentials == null ?
-                ""
-                :
+            ""
+            :
                 credentials.substring("Bearer ".length());
 
-        if (Ut.str.isBlank(apiKey))
+        if (Ut.str.isBlank(apiKey)) {
             throw new ServiceException("401-1", "apiKey를 입력해주세요.");
+        }
 
         Optional<Member> opActor = memberService.findByApiKey(apiKey);
 
-        if (opActor.isEmpty())
+        if (opActor.isEmpty()) {
             throw new ServiceException("401-1", "사용자 인증정보가 올바르지 않습니다.");
+        }
 
         return opActor.get();
     }
+
     public Member getActorByUsername(String username) {
         return memberService.findByUsername(username).get();
+    }
+
+    // 스프링 시큐리티가 이해하는 방식으로 강제 로그인 처리
+    // 임시함수
+    public void setLogin(String username) {
+        UserDetails user = new User(
+            username,
+            "",
+            List.of()
+        );
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            user,
+            user.getPassword(),
+            user.getAuthorities()
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
--- src/main/java/org/sangxxjin/javapractice/global/security/CustomAuthenticationFilter.java
+++ src/main/java/org/sangxxjin/javapractice/global/security/CustomAuthenticationFilter.java
@@ -4,16 +4,22 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.sangxxjin.javapractice.global.rq.Rq;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
+
+    private final Rq rq;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("CustomAuthenticationFilter.doFilterInternal");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        rq.setLogin("user1");
         filterChain.doFilter(request, response);
     }
 }
