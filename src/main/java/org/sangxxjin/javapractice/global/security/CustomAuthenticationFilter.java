f0af79890a866721172ab222b05ba8361b4df9d3
sangxxjin <sj_0368@naver.com>
2025-01-08 11:18:42 +0900

    feat: CustomAuthenticationFilter 를 작성하고 그것이 시큐리티가 본격적으로 작동하기 전에 작동하도록 설정

---
+++ src/main/java/org/sangxxjin/javapractice/global/security/CustomAuthenticationFilter.java
@@ -0,0 +1,19 @@
+package org.sangxxjin.javapractice.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("CustomAuthenticationFilter.doFilterInternal");
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/org/sangxxjin/javapractice/global/security/SecurityConfig.java
+++ src/main/java/org/sangxxjin/javapractice/global/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package org.sangxxjin.javapractice.global.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain baseSecurityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -14,7 +21,8 @@ public class SecurityConfig {
                 authorizeRequests
                     .requestMatchers("/h2-console/**")
                     .permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/*/posts/{id:\\d+}", "/api/*/posts", "/api/*/posts/{postId:\\d+}/comments")
+                    .requestMatchers(HttpMethod.GET, "/api/*/posts/{id:\\d+}", "/api/*/posts",
+                        "/api/*/posts/{postId:\\d+}/comments")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
@@ -29,7 +37,10 @@ public class SecurityConfig {
             .csrf(
                 csrf ->
                     csrf.disable()
-            );
+            )
+            .addFilterBefore(customAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
