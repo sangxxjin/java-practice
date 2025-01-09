8c871f84a46885666cb3173677e74a9ffd1c0866
sangxxjin <sj_0368@naver.com>
2025-01-09 12:46:21 +0900

    refactor: 테스트케이스들을 스프링 시큐리티에 맞게 수정

--- src/test/java/org/sangxxjin/javapractice/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/org/sangxxjin/javapractice/post/post/controller/ApiV1PostControllerTest.java
@@ -192,7 +192,7 @@ public class ApiV1PostControllerTest {
             .andExpect(handler().methodName("write"))
             .andExpect(status().isUnauthorized())
             .andExpect(jsonPath("$.resultCode").value("401-1"))
-            .andExpect(jsonPath("$.msg").value("apiKey를 입력해주세요."));
+            .andExpect(jsonPath("$.msg").value("사용자 인증정보가 올바르지 않습니다."));
     }
 
     @Test
@@ -297,7 +297,7 @@ public class ApiV1PostControllerTest {
             .andExpect(handler().methodName("modify"))
             .andExpect(status().isUnauthorized())
             .andExpect(jsonPath("$.resultCode").value("401-1"))
-            .andExpect(jsonPath("$.msg").value("apiKey를 입력해주세요."));
+            .andExpect(jsonPath("$.msg").value("사용자 인증정보가 올바르지 않습니다."));
     }
 
     @Test
@@ -385,7 +385,7 @@ public class ApiV1PostControllerTest {
             .andExpect(handler().methodName("delete"))
             .andExpect(status().isUnauthorized())
             .andExpect(jsonPath("$.resultCode").value("401-1"))
-            .andExpect(jsonPath("$.msg").value("apiKey를 입력해주세요."));
+            .andExpect(jsonPath("$.msg").value("사용자 인증정보가 올바르지 않습니다."));
     }
 
     @Test
@@ -451,7 +451,7 @@ public class ApiV1PostControllerTest {
             .andExpect(handler().methodName("item"))
             .andExpect(status().isUnauthorized())
             .andExpect(jsonPath("$.resultCode").value("401-1"))
-            .andExpect(jsonPath("$.msg").value("apiKey를 입력해주세요."));
+            .andExpect(jsonPath("$.msg").value("로그인이 필요합니다."));
     }
 
     @Test
