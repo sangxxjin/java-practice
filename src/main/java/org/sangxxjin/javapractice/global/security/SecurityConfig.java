b5ba4b35cf45e09b6ad3822c690748c70778be4e
sangxxjin <sj_0368@naver.com>
2025-01-09 11:16:24 +0900

    feat: 회원가입, 로그인 API 요청에 인증검사 제거

--- src/main/java/org/sangxxjin/javapractice/global/security/SecurityConfig.java
+++ src/main/java/org/sangxxjin/javapractice/global/security/SecurityConfig.java
@@ -24,6 +24,8 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.GET, "/api/*/posts/{id:\\d+}", "/api/*/posts",
                         "/api/*/posts/{postId:\\d+}/comments")
                     .permitAll()
+                    .requestMatchers("/api/*/members/login", "/api/*/members/join")
+                    .permitAll()
                     .anyRequest()
                     .authenticated()
             )
